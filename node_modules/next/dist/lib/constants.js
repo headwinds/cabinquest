ea099c3115cf43f7fa1ce9e0aa8f339716764d40
Brandon Flowers <brandonflowers@gmail.com>
2019-01-05 08:09:43 -0500

    deleted node modules

--- node_modules/next/dist/lib/constants.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var PHASE_EXPORT = exports.PHASE_EXPORT = 'phase-export';
-var PHASE_PRODUCTION_BUILD = exports.PHASE_PRODUCTION_BUILD = 'phase-production-build';
-var PHASE_PRODUCTION_SERVER = exports.PHASE_PRODUCTION_SERVER = 'phase-production-server';
-var PHASE_DEVELOPMENT_SERVER = exports.PHASE_DEVELOPMENT_SERVER = 'phase-development-server';
